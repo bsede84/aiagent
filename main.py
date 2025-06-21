c83129f6dc756c08ea1d6a12addd50a482983f4f
bsede <bsede84@gmail.com>
2025-06-21 14:16:45 -0500

    B: Set-up program to accept command prompts

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import os
+import sys
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+parser = argparse.ArgumentParser(description='A sample script that accepts command-line arguments.')
+parser.add_argument('--verbose', action='store_true', help='Enable verbose output.')
+parser.add_argument('user_prompt', nargs='+', help='The user prompt to send to the model.')
+# Parse args after all arguements have been defined
+args = parser.parse_args()
+
+if len(sys.argv) > 1:
+    user_prompt = " ".join(args.user_prompt)
+else:
+    print("No arguments provided.")
+    sys.exit(1)
+
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=messages
 )
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if args.verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
